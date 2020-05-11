c1c943cafcaaa16e0d9d740da8552742a429ea7a
Math247 <50381305+Math247@users.noreply.github.com>
2020-05-10 22:48:13 -0300

    Mais Exercícios do Curso de Delphi para Iniciantes

---
+++ Delphi 206 EditMask/MainForm.pas
@@ -0,0 +1,25 @@
+unit MainForm;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.Mask;
+
+type
+  TForm1 = class(TForm)
+    MaskEdit1: TMaskEdit;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+end.
