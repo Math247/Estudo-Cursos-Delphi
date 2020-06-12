09235dbdf922dfa60efc1e9832f6e4e6ffe2e17e
Matheus Carregal <matheuschabib@gmail.com>
2020-06-12 14:50:17 -0300

    Exercício Aula 1 POO

---
+++ Delphi401 POO/Classe.Pessoa.pas
@@ -0,0 +1,15 @@
+unit Classe.Pessoa;
+
+interface
+
+type
+  TPessoa = class
+  private //visivel somente para essa unit;
+    Idade : Integer;
+  public
+    Nome : String
+  end;
+
+implementation
+
+end.
---
+++ Delphi401 POO/View.Principal.pas
@@ -0,0 +1,43 @@
+unit View.Principal;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Classe.Pessoa;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  Pessoa : TPessoa;
+begin
+  //sempre tem que dar um Create;
+  Pessoa := TPessoa.Create;
+  try
+    //só enxergo o nome, pois a Idade eh private;
+    Pessoa.Nome := 'Matheus';
+    ShowMessage(Pessoa.Nome);
+  finally
+    //sempre destrua a classe criada, para não ocupar mémória
+    Pessoa.Free;
+  end;
+
+end;
+
+end.
